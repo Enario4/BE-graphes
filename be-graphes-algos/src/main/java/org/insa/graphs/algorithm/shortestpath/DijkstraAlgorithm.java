6eed9b9dcb1e3b6b4ec44703f72754f2ecc9faef
Enairo4 <orianeberry4@gmail.com>
2021-05-04 17:25:16 +0200

    L'algorithme de Dijkstra fonctionne

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,5 +1,9 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+import org.insa.graphs.algorithm.AbstractSolution.Status;
 import org.insa.graphs.algorithm.utils.*;
 import org.insa.graphs.model.*;
 
@@ -23,9 +27,6 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 		// Tableau de Labels 
 		Label labels_tab[] = new Label[graph.size()];
 		
-		// Tableau des prédecesseurs 
-		Arc[] arc_prec = new Arc[graph.size()];
-		
 		// Initialisation 
 		
 		for (Node node : graph.getNodes()) {
@@ -36,24 +37,71 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 		tas.insert(first);
 		first.setCost(0);
 		notifyOriginProcessed(data.getOrigin());
-		
 		// Itérations 
 		
   		while (!fini && !tas.isEmpty()) {
 	        Label x = tas.deleteMin();
-	        x.setMarque();
+	        x.setMark();
 	        notifyNodeMarked(x.getNode());
 	        
 	        //Si c'est la destination, on s'arrête
-	        if (x.getNode().compareTo(dest) == 0 {
+	        if (x.getNode().compareTo(dest) == 0) {
 	        	fini = true;
+	        }
+	        //Sinon on parcourt les successeurs
+	        for (Arc arc : labels_tab[x.getNode().getId()].getNode().getSuccessors()) {
+	        	//Si le sommet n'est pas marqué :
+	        	if (!(labels_tab[arc.getDestination().getId()].isMarked())) {
+	        		
+	        		//Si on obtient un meilleur coût
+	        		if (labels_tab[arc.getDestination().getId()].getCost() > labels_tab[x.getNode().getId()].getCost() + arc.getLength()) {
+	        			//Si deja dans le tas 
+	        			if (labels_tab[arc.getDestination().getId()].getCost() != Float.MAX_VALUE) {
+	        				tas.remove(labels_tab[arc.getDestination().getId()]);
+	        			}
+
+	          			System.out.print("mise à jour du coût\n");
+	        		//Met à jour le coût et ajoute le label au tas
+	        			labels_tab[arc.getDestination().getId()].setCost(labels_tab[x.getNode().getId()].getCost()+ arc.getLength());
+	        			tas.insert(labels_tab[arc.getDestination().getId()]);
+	        			labels_tab[arc.getDestination().getId()].setArcPrec(arc);
+	        		}
+	        	}
+	        }
+	        // Destination has no predecessor, the solution is infeasible...
+	        if (labels_tab[data.getDestination().getId()] == null) {
+	            solution = new ShortestPathSolution(data, Status.INFEASIBLE);
+	        }
+	        else {
 	        	
+	            // The destination has been found, notify the observers.
+	            notifyDestinationReached(data.getDestination());
+	            System.out.print("ligne 1\n");
+	            // Create the path from the array of predecessors...
+	            ArrayList<Arc> arcs = new ArrayList<>();
+	            System.out.print("ligne 2\n");
+	            Arc arc = labels_tab[data.getDestination().getId()].getArcPrec();
+	            System.out.print("ligne 3\n");
+	            while (arc != null) {
+	            	System.out.print("ligne 4 dans le while \n");
+	                arcs.add(arc);
+	                System.out.print("ligne 5\n");
+	                System.out.print(arc.getDestination().getId());
+	                System.out.print(arc.getOrigin().getId());
+	                arc = labels_tab[arc.getOrigin().getId()].getArcPrec();
+	                System.out.print("ligne 6\n");
+	            }
+
+	            // Reverse the path...
+	            System.out.print("ligne 7 après le while\n");
+	            Collections.reverse(arcs);
+
+	            // Create the final solution.
+	            System.out.print("ligne 8\n");
+	            solution = new ShortestPathSolution(data, Status.OPTIMAL, new Path(graph, arcs));
 	        }
   		}
         
-        //tant que tous les sommets sont pas marqués : parcourt les successeurs 
-        //Tant qu'il y a des successeurs : récupère ou crée le label 
-        //Marque le successeur si pas deja fait. Si cout plus faible : met a jour et ajoute le noeud dans le tas
         
         
         return solution;
